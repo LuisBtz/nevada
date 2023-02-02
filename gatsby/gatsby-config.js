fe61fd98c883dd6477a73081890a21a65db17461
Luis B <luisbttf@gmail.com>
2023-02-01 18:48:46 -0600

    favicon

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -4,22 +4,22 @@
 module.exports = {
   siteMetadata: {
     title: `Nevada`,
-    siteUrl: `https://www.yourdomain.tld`,
-    description: `Full Website Coming Soon`,
-    twitterUsername: `@gatsbyjs`,
+    siteUrl: `https://nevadaprojects.com/`,
+    description: `Nevada Projects Sitio Web Próximamente`,
+    twitterUsername: `@nevada.projects`,
     image: `./src/assets/screenshot.png`,
     author: `nevada`
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/assets/images/icon.png"
-    }
+        icon: 'src/assets/images/icon.png',
+      },
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./src/assets//images/"
+      "path": "./src/assets/images/"
     },
     __key: "images"
   }]
